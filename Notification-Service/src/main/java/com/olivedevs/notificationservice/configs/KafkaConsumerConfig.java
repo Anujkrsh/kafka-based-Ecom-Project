18554878558a2c682fbb190a21c61da9690c35fc
Anujkrsh <anujkrsharma@hotmail.com>
2026-01-28 21:45:48 +0530

    updated kafka Config to debug Notification Service

--- Notification-Service/src/main/java/com/olivedevs/notificationservice/configs/KafkaConsumerConfig.java
+++ Notification-Service/src/main/java/com/olivedevs/notificationservice/configs/KafkaConsumerConfig.java
@@ -11,6 +11,9 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
+import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.util.backoff.FixedBackOff;
 
@@ -25,12 +28,22 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, Object> consumerFactory() {
         JsonDeserializer<Object> deserializer = new JsonDeserializer<>(Object.class);
         deserializer.addTrustedPackages("*");
-        deserializer.setUseTypeHeaders(false);
+        deserializer.setUseTypeHeaders(true);
+
+        deserializer.setTypeMapper(new DefaultJackson2JavaTypeMapper() {{
+            setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID);
+            addTrustedPackages("*");
+            setIdClassMapping(Map.of(
+                    "PaymentEvent", com.olivedevs.notificationservice.dtos.PaymentEvent.class,
+                    "OrderCreatedEvent", com.olivedevs.notificationservice.dtos.OrderCreatedEvent.class
+            ));
+        }});
 
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-group");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
 
         return new DefaultKafkaConsumerFactory<>(
                 props,
